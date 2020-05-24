102345b23be90bed702d3d4459ea88bcc795c47c
Kevin Parmar <parmarkevinb@gmail.com>
2020-05-24 17:39:38 -0400

    Added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
+import "./style.css";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <header>
+        <h1>Convoke</h1>
+        <h3>A booster pack simulator for Magic the Gathering</h3>
+      </header>
+    </div>
+  );
 };
 
 render(<App />, document.getElementById("root"));
